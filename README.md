abe3703aae9653e915306f1eb0808015a9f3a55b
Jason Longshore <longshorej@gmail.com>
2018-10-28 20:48:18 -0500

    Use binary encoding

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ It's important to note that Depot is focused strictly on low-level storage. Repl
 ## Goals & Thoughts
 
 * Per usual, the goal here is simplicity.
-* Store data in files on disk with tight control over when data is flushed.
+* Store data in plain files on disk with tight control over when data is flushed.
 * Tolerate crashes, but do so in a lazy fashion.
 * Single-threaded writer.
 * Support the ability to rewrite sections of the queue in an atomic fashion. This is useful for implementing concepts like compaction.
@@ -19,15 +19,15 @@ It's important to note that Depot is focused strictly on low-level storage. Repl
 * Synchronous file I/O. Given the poor state of AIO on Linux, defer to higher level abstractions to emulate asynchronous behavior. For instance, a dedicated group of threads can be used to interact with Depot.
 * Network support is left to higher level libraries.
 * Topics, or partitioning of items, may be supported natively in Depot by composing its underlying components. TBD.
-* Base91 may be used for encoding data, instead of base64, time permitting.
+* Potentially support data integrity measures. CRC for each item is being considered, at the cost of 4 bytes of additional fixed overhead per stored item.
 
 ## FAQ
 
-### How does Depot store data?
+### How does Depot store data? What's the overhead?
 
-Depot stores its data in plain files. Depot's higher level interface composes a *QueueSection*, which stores 65536 items per file.
+Depot stores its data in plain files, and uses a binary encoding to store its data. An escape mechanism handles collisions on the record separator and failure bytes.
 
-Depot uses UTF-8 encoded files to store its data. Each entry is delimited by *\n* (0x0A) characters, and each entry consists of its data, encoded as base64, a field separator *\*, and its id data (16 bit integer, big endian). The id is specific to a *QueueSection*, and sections themselves are ordered and assigned ids, which are taken into account when determing an items' 64-bit id value.
+Each record stored in Depot costs a constant 5 bytes of overhead, plus ~2% overhead for the encoding mechanism.
 
 ### How does Depot deal with crashes while writing data?
 
@@ -37,13 +37,32 @@ However, if it isn't a newline (and the file is not empty), Depot assumes that t
 
 ### How fast is Depot?
 
-That depends on the machine and data size, but it seems to be pretty fast. A Lenovo Thinkpad, i7-6600U, with a consumer-grade SSD achieves over 10 million writes per second, and 5 million reads per second, for small payloads.
+The low level primitive, *QueueSection*, is largely limited by disk I/O speed. For a very flawed initial test, given a Lenovo Thinkpad, i7-6600U, with a consumer-grade SSD, 50 byte payloads, about 4.7M reads/sec can be performed by a single reader. This translates to ~300MB/sec. For a writer, given the same constraints, about 1.7M writes/sec, translating to ~110MB/sec. Be sure to take it with a grain of salt.
+
+The higher level interface hasn't been written yet, but it shouldn't incur much additional overhead other than switching between files.
 
 Given its append only design, it should also perform well with "spinning rust" disks.
 
 ### Does it support multiple concurrent writers?
 
-Multiple concurrent writers are not supported. A library such as [semalock](https://github.com/longshorej/semalock) can be used if coordination between processes is required.
+Multiple concurrent writers are not supported. A library such as [semalock](https://github.com/longshorej/semalock) can be used if coordination between processes is required. It's better to use messaging and a single writer if possible though.
+
+### Does Depot support removing records?
+
+Conceptually, yes, but this hasn't been implemented yet. Given that queue's are split into files that contain a bounded number of items, each of these files can be rewritten and then atomically renamed over the old section.
+
+### Java and Scala?
+
+Support is planned via JNI, see the [jvm](jvm) which is working toward a proof of concept to use Depot from Java and Scala code.
+
+## Impl Notes
+
+* A *QueueSection* can have upto 1,048,576 items stored in it, which means ~1GB files if each item is 1KB. An item can be upto 64KB in length, meaning the maximum size for a given section is 64GB. This is important to keep removal / rewriting workable, as entire sections need to be rewritten.
+* 36 bits are used to store file offsets, and 28 bits are used to store the section number. This can be encoded as 8 hex characters, with a maximum value of 10000000 (hex).
+* A *Queue* stores its data in *QueueSection*s which are themselves assigned ids. Once a queue is full, a new one is created with the next id. IDs are encoded as hex strings that are zero-padded to 6 digits. This is grouped into two 2 digit groups, the parent directories, and a 3 digit hex encoding as the final name. For instance, given ABCDEF12, it would be stored in `<storage-dir>/ab/cd/ef12`.
+* This means that there can a total of 268,435,456 files on the system, each containing 1,048,576 items, for a total of 17,592,186,044,416 items. If creating 1M items per second, this would be exh
+* Ammendment: probably will change most of the above
+
 
 ## License
 
--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,6 @@ use std::io::prelude::*;
 use std::io::{BufReader, BufWriter, Cursor, SeekFrom};
 use std::path::PathBuf;
 
-const MARKER_FAILURE: char = '-';
-const MARKER_FIELD: char = '\\';
-const MARKER_NEWLINE: char = '\n';
-
 pub enum Decoded {
     Item(Item),
     TruncatedItem(TruncatedItem),
@@ -19,17 +15,25 @@ pub enum Decoded {
 
 #[derive(Debug)]
 pub struct Item {
-    id: u32,
-    data: Vec<u8>,
+    pub id: u32,
+    pub data: Vec<u8>,
 }
 
 pub struct TruncatedItem {
-    data: Option<Vec<u8>>,
+    pub data: Vec<u8>,
 }
 
 pub struct Reader {
     path_buf: PathBuf,
 }
+
+type RecordId = u32;
+
+const MARKER_ESCAPE: u8 = '\\' as u8;
+const MARKER_SEPARATOR: u8 = '\n' as u8;
+const MARKER_SEPARATOR_REMAP: u8 = '$' as u8;
+const MARKER_FAIL: u8 = '-' as u8;
+const MARKER_FAIL_REMAP: u8 = '.' as u8;
 
 impl Reader {
     pub fn new<S: AsRef<str>>(path: S) -> io::Result<Reader> {
@@ -40,13 +44,15 @@ impl Reader {
 
     /// Returns an `Iterator` of items stored on the queue. This skips over
     /// items that were only partially written due to crash or power loss,
-    pub fn stream(&self) -> io::Result<impl Iterator<Item = io::Result<Item>>> {
+    ///
+    /// If an offset is provided, streaming will resume from that position.
+    pub fn stream(&self, offset: Option<u64>) -> io::Result<impl Iterator<Item = io::Result<Item>>> {
         let file = self.open_file()?;
         let buf_reader = BufReader::new(file);
 
         let iterator = buf_reader
-            .lines()
-            .map(|p| p.and_then(|l| Reader::parse_item(l, false)))
+            .split(MARKER_SEPARATOR)
+            .map(|p| p.and_then(Self::parse_item))
             .filter_map(|i| match i {
                 Ok(Decoded::Item(i)) => Some(Ok(i)),
                 Ok(Decoded::TruncatedItem(_)) => None,
@@ -62,18 +68,22 @@ impl Reader {
     /// This is useful for e.g. replication systems that may be built
     /// on this queue as perhaps parttially written data is of use as
     /// well.
-    pub fn stream_with_truncated(&self) -> io::Result<impl Iterator<Item = io::Result<Decoded>>> {
+    ///
+    /// If an offset is provided, streaming will resume from that position.
+    pub fn stream_with_truncated(&self, offset: Option<u64>) -> io::Result<impl Iterator<Item = io::Result<Decoded>>> {
         let file = self.open_file()?;
         let buf_reader = BufReader::new(file);
 
         let iterator = buf_reader
-            .lines()
-            .map(|p| p.and_then(|l| Reader::parse_item(l, true)));
+            .split(MARKER_SEPARATOR)
+            .map(|p| p.and_then(Self::parse_item));
 
         Ok(iterator)
     }
 
+    /// Opens a file for reading
     fn open_file(&self) -> io::Result<File> {
+        // @TODO consider removing the write(true) -- it's only needed for create(true).
         OpenOptions::new()
             .create(true)
             .write(true)
@@ -81,68 +91,92 @@ impl Reader {
             .open(&self.path_buf)
     }
 
-    fn parse_item<S: AsRef<str>>(data: S, copy_truncated: bool) -> io::Result<Decoded> {
-        let data = data.as_ref();
-        let mut parts = data.split(MARKER_FIELD);
+    /// Parses an item that was read off of the disk.
+    fn parse_item(mut data: Vec<u8>) -> io::Result<Decoded> {
+        let len = data.len();
+        let id_length = 4;
 
-        let data_part = parts.next();
-        let id_part = parts.next();
-        let empty_part = parts.next();
+        let mut escaped = false;
+        let mut i = 0;
+        let mut t = 0;
 
-        match (data_part, id_part, empty_part) {
-            (Some(_), Some(id_bytes), None) if id_bytes.ends_with(MARKER_FAILURE) => {
-                let item = if copy_truncated {
-                    TruncatedItem {
-                        data: Some(data.as_bytes().to_vec()),
-                    }
-                } else {
-                    TruncatedItem { data: None }
-                };
+        // First, decode our data in place
 
-                Ok(Decoded::TruncatedItem(item))
-            }
+        while i < len {
+            let byte = data[i];
 
-            (Some(data_bytes), Some(id_bytes), None) => {
-                // @TODO FIXME unwraps
-
-                let id_data = base64::decode(&id_bytes).map_err(|_| {
+            if escaped && byte == MARKER_FAIL_REMAP {
+                data[i - t] = MARKER_FAIL;
+                escaped = false;
+            } else if escaped && byte == MARKER_SEPARATOR_REMAP {
+                data[i - t] = MARKER_SEPARATOR;
+                escaped = false;
+            } else if escaped && byte == MARKER_ESCAPE {
+                data[i - t] = MARKER_ESCAPE;
+                escaped = false;
+            } else if escaped {
+                return Err(
                     io::Error::new(
                         io::ErrorKind::InvalidData,
-                        "cannot parse file, error reading id",
+                        format!("cannot parse file, invalid byte {} after escape", byte),
                     )
-                })?;
-
-                let mut id_cursor = Cursor::new(id_data);
-                let id = id_cursor.read_u32::<BigEndian>().unwrap();
-
-                let data = base64::decode(&data_bytes).map_err(|_| {
-                    io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "cannot parse file, error reading data",
-                    )
-                })?;
-
-                Ok(Decoded::Item(Item { id, data }))
+                );
+            } else if byte == MARKER_ESCAPE {
+                escaped = true;
+                t += 1;
+            } else {
+                data[i - t] = data[i];
             }
 
-            _ => Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                "cannot parse file, error reading data",
-            )),
+            i += 1;
         }
+
+        let new_length = len - id_length - t;
+
+        if new_length < id_length {
+            return Err(
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "cannot parse file, error reading id",
+                )
+            );
+        }
+
+        if len > 0 && data[len -1] == MARKER_FAIL {
+            return Ok(Decoded::TruncatedItem(TruncatedItem { data }));
+        }
+
+        // the ID is now the first four bytes, big endian, so extract it
+
+        let id = {
+            let mut id_cursor = Cursor::new(&data[0 .. id_length]);
+            id_cursor.read_u32::<BigEndian>()?
+        };
+
+        // finally, shift everything over and truncate the item's vector.
+
+        let mut i = id_length;
+
+        while i < len {
+            data[i - id_length] = data[i];
+            i += 1;
+        }
+
+        data.truncate(new_length);
+
+        Ok(Decoded::Item(Item { id, data }))
     }
 }
 
 pub struct Writer {
     buffer: BufWriter<File>,
     id_buffer: Vec<u8>,
-    item_buffer: [u8; 8192],
+    item_buffer: [u8; 2],
     next_id: u32,
 }
 
 impl Writer {
     /// Creates a new Writer from the given file.
-    /// @TODO use multiple files to allow rewriting sections
     pub fn new(path: &PathBuf) -> io::Result<Writer> {
         let mut file = OpenOptions::new()
             .create(true)
@@ -160,19 +194,22 @@ impl Writer {
             let mut eof = vec![0u8; 1];
             file.read_exact(&mut eof)?;
 
-            let length = if eof[0] == MARKER_NEWLINE as u8 {
+            let length = if eof[0] == MARKER_SEPARATOR as u8 {
                 length
             } else {
                 // We must have crashed before flushing to disk
                 // Thus, we have no choice but to append a marker
                 // and the newline. If this crashes, on the next
                 // recovery we'd simply have n markers. Since all
-                // items are base64 encoded, - and \n cannot be
+                // items are encoded according to our scheme, - and \n cannot be
                 // part of the payload.
                 // Since we're append only, this could allow other
                 // processes to inspect failed elements, i.e. we
                 // don't actively remove any data.
-                file.write_all(&[MARKER_FAILURE as u8, MARKER_NEWLINE as u8])?;
+                // Note that we append MARKER_FAIL twice as its possible
+                // we crashed with the last value being an escape character,
+                // so the first could potentially be escaped.
+                file.write_all(&[MARKER_FAIL as u8, MARKER_FAIL as u8, MARKER_SEPARATOR as u8])?;
                 file.flush()?;
                 length + 2
             };
@@ -183,9 +220,11 @@ impl Writer {
         };
 
         let buffer = BufWriter::new(file);
-        // FIXME can id_buffer by a slice? don't see how with byteorder crate, see #99
-        let id_buffer = Vec::with_capacity(4);
-        let item_buffer: [u8; 8192] = [0; 8192];
+        let id_buffer = Vec::with_capacity(4); // FIXME can id_buffer by a slice? don't see how with byteorder crate, see #99
+
+        // Upto 2 bytes are needed for each step -- the relevant data and possibly
+        // an escape.
+        let item_buffer: [u8; 2] = [0; 2];
 
         let next_id = maybe_last_id.unwrap_or_else(|| 0) + 1;
 
@@ -199,9 +238,8 @@ impl Writer {
 
     /// Append the given data to the file.
     pub fn append(&mut self, data: &[u8]) -> io::Result<()> {
-        // technically, 4/3 the size is all that's needed, but for
-        // we simply require data to fit in half the buffer size
-        // for simplicity
+        // the worst case is that every byte is escaped, so we need
+        // twice the buffer size
 
         if data.len() > self.item_buffer.len() / 2 {
             return Err(io::Error::new(
@@ -210,34 +248,60 @@ impl Writer {
             ));
         }
 
-        let bytes_written =
-            base64::encode_config_slice(data, base64::STANDARD_NO_PAD, &mut self.item_buffer);
-
         self.id_buffer.clear();
         self.id_buffer.write_u32::<BigEndian>(self.next_id)?;
 
-        self.item_buffer[bytes_written] = MARKER_FIELD as u8;
+        let bytes = [&self.id_buffer[0 .. 4], data];
 
-        let id_bytes_written = base64::encode_config_slice(
-            &self.id_buffer,
-            base64::STANDARD_NO_PAD,
-            &mut self.item_buffer[bytes_written + 1..],
-        );
+        let mut i = 0;
 
-        let bytes_slice = &self.item_buffer[0..bytes_written + 1 + id_bytes_written];
+        while i < bytes.len() {
+            let bs = bytes[i];
 
-        self.buffer.write_all(bytes_slice)?;
+            for byte in bs {
+                match *byte {
+                    MARKER_ESCAPE => {
+                        self.item_buffer[0] = MARKER_ESCAPE;
+                        self.item_buffer[1] = MARKER_ESCAPE;
+                        self.buffer.write_all(&self.item_buffer[0 .. 2])?;
+                    }
 
-        self.buffer.write_all(&[MARKER_NEWLINE as u8])?;
+                    MARKER_SEPARATOR => {
+                        self.item_buffer[0] = MARKER_ESCAPE;
+                        self.item_buffer[1] = MARKER_SEPARATOR_REMAP;
+                        self.buffer.write_all(&self.item_buffer[0 .. 2])?;
+                    }
+
+                    MARKER_FAIL => {
+                        self.item_buffer[0] = MARKER_ESCAPE;
+                        self.item_buffer[1] = MARKER_FAIL_REMAP;
+                        self.buffer.write_all(&self.item_buffer[0 .. 2])?;
+                    }
+
+                    other => {
+                        self.item_buffer[0] = other;
+                        self.buffer.write_all(&self.item_buffer[0 .. 1])?;
+                    }
+                };
+            }
+            i += 1;
+        }
+
+        self.buffer.write_all(&[MARKER_SEPARATOR as u8])?;
 
         self.next_id += 1;
 
         Ok(())
     }
 
-    /// Determines the last id that was written
+    pub fn is_empty(&self) -> bool {
+        self.next_id == 1
+    }
+
+    /// Determines the last id that was written, or `None` if
+    /// empty.
     pub fn last_id(&self) -> Option<u32> {
-        if self.next_id == 1 {
+        if self.is_empty() {
             None
         } else {
             Some(self.next_id - 1)
@@ -250,36 +314,11 @@ impl Writer {
     }
 }
 
-#[test]
-fn test_read() {
-    let reader = Reader::new("/home/longshorej/test.what").unwrap();
-
-    let mut iter = reader.stream().unwrap();
-
-    let mut i = 0;
-    while let Some(item) = iter.next() {
-        //println!("{:?}", item);
-        i += 1;
-    }
-
-    println!("read: {}", i);
-}
-
-//#[test]
-fn test() {
-    let mut appender = Writer::new(&PathBuf::from("/home/longshorej/test.what")).unwrap();
-
-    let mut i = 0;
-    let data = [1, 2, 3, 4];
-
-    while i < 10_000_000 {
-        appender.append(&data).unwrap();
-        i += 1;
-    }
-
-    appender.sync().unwrap();
-}
-
+/// Given and open file and its total length, extract the last id
+/// that was written. This should perform well for the average case,
+/// but in the event of repeated power outages, with no writes since,
+/// this could perform quite slowly as it needs to read the file
+/// backwards and that doesn't work well with file I/O in general.
 fn last_id(file: &mut File, length: u64) -> io::Result<Option<u32>> {
     let chunk_size = 8192;
 
@@ -314,42 +353,26 @@ fn last_id(file: &mut File, length: u64) -> io::Result<Option<u32>> {
         {
             let mut i = 0;
             let mut iterator = current_line
-                .split(|b| b == &(MARKER_NEWLINE as u8))
+                .split(|b| b == &(MARKER_SEPARATOR as u8))
                 .rev()
                 .peekable();
 
             while let Some(l) = iterator.next() {
                 if iterator.peek().is_none() {
                     next_current_line = l.to_vec();
-                } else if l.ends_with(&['-' as u8]) {
+                } else if l.ends_with(&[MARKER_FAIL as u8]) {
                     bad_ids += 1;
-                } else if !l.is_empty() {
-                    match l.split(|b| b == &(MARKER_FIELD as u8)).last() {
-                        Some(bytes) => {
-                            let id_data = base64::decode(&bytes).map_err(|_| {
-                                io::Error::new(
-                                    io::ErrorKind::InvalidData,
-                                    "cannot parse file, error reading id",
-                                )
-                            })?;
-                            let mut id_cursor = Cursor::new(id_data);
-                            let id = id_cursor.read_u32::<BigEndian>()?;
-                            let next_id = id + bad_ids;
+                } else if l.len() >= 4 {
+                    let id_data = &l[0 .. 4];
+                    let mut id_cursor = Cursor::new(id_data);
+                    let id = id_cursor.read_u32::<BigEndian>()?;
+                    let next_id = id + bad_ids;
 
-                            return Ok(Some(next_id));
-                        }
-
-                        None => {
-                            return Err(io::Error::new(
-                                io::ErrorKind::InvalidData,
-                                "cannot parse file, line is missing field separator",
-                            ));
-                        }
-                    }
+                    return Ok(Some(next_id));
                 } else if i > 0 {
                     return Err(io::Error::new(
                         io::ErrorKind::InvalidData,
-                        "cannot parse file, empty line",
+                        "cannot parse file, missing data",
                     ));
                 } else if !l.is_empty() {
                     return Err(io::Error::new(
@@ -368,3 +391,38 @@ fn last_id(file: &mut File, length: u64) -> io::Result<Option<u32>> {
 
     Ok(if bad_ids == 0 { None } else { Some(bad_ids) })
 }
+
+#[test]
+fn test_read() {
+    return;
+    use std::str;
+    let reader = Reader::new("/home/longshorej/test.what").unwrap();
+
+    let mut iter = reader.stream(None).unwrap();
+
+    let mut i = 0;
+    while let Some(item) = iter.next() {
+        //println!("item: {:?}", str::from_utf8(&item.unwrap().data).unwrap());
+        i += 1;
+    }
+
+    println!("read: {}", i);
+}
+
+#[test]
+fn test() {
+    //return;
+    let mut appender = Writer::new(&PathBuf::from("/home/longshorej/test.what")).unwrap();
+
+    let mut i = 0;
+
+    while i < 20_000_000 {
+        let message = format!("the quick brown fox jumped over the lazy dog, -\n #{}", i);
+        let data = message.as_bytes();
+        appender.append(&data).unwrap();
+        i += 1;
+    }
+
+    appender.sync().unwrap();
+}
+
